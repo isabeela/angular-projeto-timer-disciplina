a4ce77c782a3ad4a77c5d0bd723c8bfa578615a5
isabeela <isabeelabg@gmail.com>
2021-09-10 17:00:00 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component, VERSION } from "@angular/core";
-import { TimerService } from "./timer.service";
+import { Component, VERSION } from '@angular/core';
+import { TimerService } from './timer.service';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = "Timer e Disciplinas";
+  name = ' Projeto Timer e Disciplinas';
 
   constructor(public timer: TimerService) {
     this.timer.start(1000);
